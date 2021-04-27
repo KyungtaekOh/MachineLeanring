04c4c27955b9fca8882b5323a7c8e7c831336197
Taek <rudxor5026@gmail.com>
2021-04-27 21:54:07 +0900

    Preparing for Learning

--- YOLOv1-keras/train.py
+++ YOLOv1-keras/train.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from tensorflow.keras.callbacks import ModelCheckpoint
 import os
 import utils, yolo, loss
 
@@ -14,20 +15,50 @@ classes = ['person', # Person
            'bottle', 'chair', 'dining table', 'potted plant', 'sofa', 'tv/monitor' # Indoor
            ]
 
-num_classes = len(classes)
-grid_size = 7
-num_bboxes = 2
+info_list = utils.get_info(annotation, classes) # 6045
+batch_size = 4
+epoch = 100
+param = {'trial'        :   1  ,          ########## Modify before train ##########
+         'model_name'   :   'YOLOv1',
+         'grid_size'    :   7,
+         'num_bboxes'   :   2,
+         'num_classes'  :   len(classes),
+         'batch'        :   batch_size,
+         'epoch'        :   epoch,
+         'trn_val_len'  :   5000,
+         'trn_val_rate' :   0.3,
+         'test_len'     :   len(info_list)-5000,
+         'lr'           :   1e-4,
+         'lr_schedule'  :   utils.LR_SCHEDULE,
+         'momentum'     :   9,
+         'weight_decay' :   5e-4,
+         }
 
-info_list = utils.get_info(annotation, classes)
-print(len(info_list))
-train_list = info_list[:4000]
-valid_list = info_list[4000:]
+##############
+# Split Data #
+##############
+train_valid_list = info_list[:5000]
+test_list = info_list[5000:]
 
-# print(train_valid_list[:6])
-x, y = utils.data_generation(train_list[:16], images)
+train_data_list, valid_data_list = utils.train_test_split(train_valid_list) # 3500, 1500
+train_generator = utils.DataGenerator(train_data_list, batch_size, images, param['grid_size'], param['num_bboxes'], param['num_classes'])
+valid_generator = utils.DataGenerator(valid_data_list, batch_size, images, param['grid_size'], param['num_bboxes'], param['num_classes'])
+step_size_train = train_generator.n // train_generator.batch_size
+step_size_valid = valid_generator.n // valid_generator.batch_size
 
+#############
+# Callbacks #
+#############
+custom_scheduler = utils.custom_lr_scheduler(utils.lr_schedule)
+save_file_name = "{}_t{}".format(param['model_name'], param['trial'])
+history = tf.keras.callbacks.CSVLogger('/home/cvlab09/kyung-taek/detection/YOLOv1/history/'+save_file_name+'_history.txt', separator="\t", append=True)
+checkpoint = ModelCheckpoint('/home/cvlab09/kyung-taek/detection/YOLOv1/weights/'+save_file_name+'.h5',
+                             monitor='val_loss',
+                             verbose=1,
+                             save_best_only=True)
 
-
-
-
-
+#########
+# Model #
+#########
+model = yolo.YOLOv1()
+model.summary()
