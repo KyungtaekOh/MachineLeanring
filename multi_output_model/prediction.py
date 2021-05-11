a6598fe2351f4b766bf17cc97092c7f50759c5ed
Kyungtaek Oh <48934601+OhKyungTaek@users.noreply.github.com>
2021-05-11 15:03:37 +0900

    Update prediction.py

--- multi_output_model/prediction.py
+++ multi_output_model/prediction.py
@@ -10,22 +10,14 @@ from PIL import Image
 os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
 os.environ["CUDA_VISIBLE_DEVICES"] = '1'
 
-data_list = tools.csv2arr(file_path=r'/home/cvlab09/kyung-taek/cnn/segmentation/test.txt',
+data_list = tools.csv2arr(file_path=r'test.txt',
                           desired_cols=[0, 1, 2])
 
-img_dir = r'/home/cvlab09/kyung-taek/cnn/segmentation/test/'
-mask_dir = r'/home/cvlab09/kyung-taek/cnn/segmentation/test_GT/'
+img_dir = r'test/'
+mask_dir = r'test_GT/'
 output_dir = r'output_crop/'
 
 model = model.getModel()
-# model = model_v2.getModel_v2()
-# weight = r'/home/cvlab09/kyung-taek/cnn/saved_weight/Unet_V4_ver2_t5.h5'
-# model.load_weights(weight)
-
-# crop_out = model.layers[33]     # crop layer
-# features_list = [layer.output for layer in model.layers[:34]]
-# model2 = tf.keras.Model(inputs = model.input, outputs = features_list[-1])
-# pred = tools.Prediction(data_list, img_dir, mask_dir, output_dir, model, save_bool=True)
 
 pred = tools.Prediction(data_list, img_dir, mask_dir, output_dir, model, save_bool=False)
 print('MIoU :', pred.iou.numpy())
